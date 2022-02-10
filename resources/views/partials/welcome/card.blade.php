b0f375c1c335bf1613b45a58a1a847bb74e2e13c
CactusB0y <68423704+CactusB0y@users.noreply.github.com>
2022-02-10 09:58:50 +0100

    structure

---
+++ resources/views/partials/welcome/card.blade.php
@@ -0,0 +1,14 @@
+<section>
+    <div class="container">
+        @foreach ($fruits as $fruit)
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">{{$fruit}}</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        @endforeach
+    </div>
+</section>
